728407bc8ccb5fd8c79d086c17b0e755f220a831
mitchwadair <mitchwadair@gmail.com>
2019-11-09 18:27:35 -0500

    maximise app window on start

--- public/electron.js
+++ public/electron.js
@@ -9,19 +9,20 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-        width: 900,
-        height: 680,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        show: false,
     });
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  if (isDev) {
-        // Open the DevTools.
-        //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-        mainWindow.webContents.openDevTools();
-  }
-  mainWindow.on('closed', () => mainWindow = null);
+    mainWindow.maximize();
+    mainWindow.show();
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+    if (isDev) {
+            // Open the DevTools.
+            //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+            mainWindow.webContents.openDevTools();
+    }
+    mainWindow.on('closed', () => mainWindow = null);
 }
 
 app.on('ready', createWindow);
