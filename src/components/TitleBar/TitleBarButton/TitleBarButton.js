48667b9767a581f428587029bc08e354f7b719c1
mitchwadair <mitchwadair@gmail.com>
2019-11-09 20:53:39 -0500

    remove whitespace

--- src/components/TitleBar/TitleBarButton/TitleBarButton.js
+++ src/components/TitleBar/TitleBarButton/TitleBarButton.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './titlebarbutton.scss';
 
 export default function TitleBarButton({func, icon, color="black"}) {
-
     return (
         <div className={"bb-title-bar-button"} onClick={func}>
             <div className={"bb-title-bar-button-image"} style={{'--url': `url(${icon}`, '--color': color}}/>
