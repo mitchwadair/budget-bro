e3cbda872db44a43c92f9619375979166c24973b
mitchwadair <mitchwadair@gmail.com>
2019-11-26 18:11:16 -0500

    BB #9 start layout

--- src/components/Pages/Dashboard/Dashboard.js
+++ src/components/Pages/Dashboard/Dashboard.js
@@ -17,11 +17,17 @@ const {prefix} = settings;
 export default function Dashboard(props) {
     const profileFolder = props.match.params.id;
     const history = useHistory();
+    const currentDir = remote.app.getAppPath();
+    const dataDir = path.join(currentDir, 'data');
+    const profileDataDir = path.join(dataDir, profileFolder);
+    const profileData = JSON.parse(fs.readFileSync(path.join(profileDataDir, "profile_data.json")));
+    const date = new Date();
 
     return (
         <Page>
-            <Logo width={'7rem'} height={'7rem'}/>
-            {profileFolder}
+            <div className={`${prefix}-dashboard-title`}><Logo width={'7rem'} height={'7rem'}/><span>Dashboard</span></div>
+            <div className={`${prefix}-dashboard-name`}>{profileData.profile_name}</div>
+            <div>{date.toString()}</div>
             <Button onClick={() => {history.push('/')}}>Go Back</Button>
         </Page>
     );
